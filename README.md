f161480a4daba582973420ff078399fabacf067d
Sulu iss <michaelwilson96@outlook.com>
2020-11-10 16:39:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# Slim Starter Kit v0.3.5   
+
+SlimStarterkit uses a combination of a Sass architecture for quick and unopinionated style starter, handlebars for templating, `.htaccess` and `404` file templates, and an automated server/build process using Gulp. This kit is quick, unopinionated, and provides a simple starting point for website projects. 
+
+### [Check out the website](http://simongooder.com/slim-starterkit)
+
+## Start developing using Gulp  
+
+1. Open Terminal  
+2. Navigate to the project folder  
+3. `npm i` installs the dependencies (You only need to do this once)  
+4. `gulp` starts the server  
+5. This process will automatically open the site in your default browser  
+6. Write code. Enjoy browser refreshing and live CSS injection.   
+7. Use `gulp build` for production build
+8. Upload the content from the `/dist` folder to your hosting
+
+## Templating with Handlebars
+1. Create partial files inside the `/partials` folder, suffix file with `.hbs`
+2. Reference a partial using `{{> partial-name}}`
+3. Create new pages directly in the `/src` directory
+4. Read the [Handlebars documentation](https://handlebarsjs.com/ "Handlebarsjs.com) to learn more about using the library.
+
+
+## Recommendations 
+
+1. Check your finished site against [Front-End Checklist](https://github.com/thedaviddias/Front-End-Checklist)
+2. If you need a CMS, or website management system (WMS) for your project, check out [Pagesimple.io](http://pagesimple.io)
+    - Build a `theme.json` file
+    - Submit site to [Pagesimple.io](http://pagesimple.io)
+3. Attach GA or some sort of analytics tracking
+4. Launch your site!
+
+
+----
+
+## ToDo
+
+- [ ] Sitemap generator?
+- [ ] Integrate HTML minifier
+- [ ] Update `documentation` to provide information about site structure and templating
+- [ ] Create build task to generate `theme.json` file
+
+
+## Changelog
+
+__Updates__ - *v0.3.5*
+- Naming conventions are now consistent (dash-separated class names)
+- Remove loop from `globalMeta` 
+- Add `-hide-bp-sm` and `-show-bp-sm` classes
+- Move `/img` folder out out `/assets` because it's not necessary
+
+__Updates__ - *v0.3.0* 
+- Integrate Handlebars templating system to support usage of partials and separate data
+- Add `assets` folder to `src`, move `img` folder into `assets`
+- Template all meta data for ease of use. Just fill out the `data.json` file to fill data across a whole template
+- Update documentation language
+- Add navigation sidebar to documentation
+- Add `.htaccess` example
+
+__Updates__ - *v0.2.0*
+- Moved to Gulp for faster build process
+- Added image compression to build process
+- Added SVG sprite builder to build process
+- More complete `container` classes
+- More complete set of `media-queries` and breakpoints
+- Cleaner class naming conventions
+- Button utlity classes
+
+__Updates__ - *v0.1.7*
+- A better build process for production files
+- Toggle component can now specify targets
+- Toggleable navigation menu (mobile)
+
+__Updates__ - *v0.1.4*
+- Styled buttons, tables, cards
+- Defined basic typography scale
+- Designed color functions for color palette generation
+- Added `container` classes
+- Added `positioning` classes
+- Added `responsive-break` classes to grid system
+
+__Updates__ - *v0.1.1*
+- Introduced new SCSS structure
+- Commencing planning phase
+
+__Updates__ - *v0.0.8*
+- Updated `media-query` mixins to reflect `min` values on defaults mixins.
+- Added a mobile navigation menu to the HTML boilerplate
+- Added Twitter Card meta, and standard meta tags
+- Moved grid system to Flex
+- Added default colors
+- Added new files to the SCSS structure for `containers`, `colors`, `typography`.
+
+__Updates__ - *v0.0.5*
+- Removed Grunt Bake dependency - because it's old, and Jekyll is awesome
+- Cleaned up GruntFile.js
+- Re-wrote the `README.md`
+
+__Updates__ - *v0.0.4*
+- Cleaned up GruntFile.js
+- Removed the pretentious `readme.md`
+- Modified SCSS Reset
+- Updated from Autoprefixer-Core to Autoprefixer PostCSS Module
---
+++ gulpfile.js
@@ -0,0 +1,288 @@
+/* ================
+// Required Plugins
+// ============= */
+
+var gulp = require('gulp'),
+    reload = require('require-reload')(require),
+    packageJSON = require('./package.json'),
+    browserSync = require('browser-sync').create(),
+    del = require('del'),
+    minimist = require('minimist'),
+    gulpif = require('gulp-if'),
+    cached = require('gulp-cached'),
+    changed = require('gulp-changed'),
+    postcss = require('gulp-postcss'),
+    sass = require('gulp-sass'),
+    sassGlob = require('gulp-sass-glob'),
+    cssnano = require('gulp-cssnano'),
+    extReplace = require('gulp-ext-replace'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    svgSprite = require('gulp-svg-sprite'),
+    autoprefixer = require('autoprefixer'),
+    handlebars = require('gulp-compile-handlebars'),
+    tap = require('gulp-tap'),
+    fs = require("fs"),
+    path = require("path");
+
+
+/* ================
+// Setup Environment
+// ============= */
+
+var dist = 'dist/',
+    src = 'src/',
+    assets = 'assets/'
+production = true;
+
+var options = minimist(process.argv.slice(2));
+
+if (options.production) production = true;
+
+
+/* ================
+// Compile Sass
+// ============= */
+
+gulp.task('sass', function () {
+
+    var plugins = [
+        autoprefixer({ browsers: ['> 0.5%', 'ie > 10'], cascade: false })
+    ];
+
+    return gulp
+        .src('scss/main.scss')
+        .pipe(sassGlob())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(plugins))
+        .pipe(cssnano())
+        .pipe(gulp.dest(dist + 'css'))
+        .pipe(browserSync.stream());
+});
+
+/* ================
+// Compile JS
+// ============= */
+
+gulp.task('js', function () {
+
+    return gulp.src([
+        'src/js/**/*.js'
+    ])
+        .pipe(concat('site.js'))
+        .pipe(gulpif(production, uglify({
+            mangle: false
+        })))
+        .pipe(gulpif(production, extReplace('.min.js')))
+        .pipe(gulp.dest(dist + 'js'))
+        .pipe(browserSync.stream());
+});
+
+
+/* ================
+// Move files
+// ============= */
+
+gulp.task('move-templates', function () {
+    return gulp.src([
+        'src/**/*.html',
+        'src/**/*.json',
+        '!src/**/data.json'
+    ])
+        .pipe(gulp.dest(dist))
+})
+
+gulp.task('move-assets', function () {
+    return gulp.src([
+        'src/assets/**',
+        '!src/assets/svg',
+        '!src/assets/img'
+    ])
+        .pipe(gulp.dest(dist))
+})
+
+
+/* ================
+// Compile Handlebars Templates
+// ============= */
+gulp.task('compile-templates', function () {
+
+    var templateData = JSON.parse(fs.readFileSync("src/data.json"));
+    var theFileName;
+
+    options = {
+        //ignorePartials: true, 
+        batch: ['./src/partials'],
+        helpers: {
+            url: function (str) {
+                return str.replace(/\s+/g, '-').toLowerCase();
+            },
+            filename: function () {
+                return theFileName
+            }
+        }
+    }
+
+    return gulp.src('src/*.hbs')
+        .pipe(tap(function (file, t) {
+            var fullPath = path.basename(file.path);
+            var trimPath = fullPath.replace(".hbs", ".html");
+            theFileName = trimPath;
+        }))
+        .pipe(handlebars(templateData, options))
+        .pipe(extReplace('.html'))
+        .pipe(gulp.dest(dist));
+});
+
+
+/* ================
+// Optimize Images
+// ============= */
+
+gulp.task('images', function () {
+
+    try {
+        return gulp.src('src/assets/img/**/*')
+            .pipe(changed(dist + 'img'))
+            .pipe(imagemin())
+            .pipe(gulp.dest(dist + 'img'))
+            .pipe(browserSync.stream());
+    } catch (e) {
+        console.error("Image minification failed. Error: ", e)
+    }
+
+});
+
+
+/* ================
+// Create SVG Sprite
+// ============= */
+
+gulp.task('sprite', function () {
+
+    return gulp.src('src/assets/svg/*.svg')
+        .pipe(svgSprite({
+            mode: {
+                inline: true,
+                symbol: {
+                    dest: "svg",
+                    sprite: "sprite.svg",
+                }
+            },
+            svg: {
+                xmlDeclaration: true,
+                doctypeDeclaration: true,
+                dimensionAttributes: false
+            }
+        }))
+
+        .pipe(gulp.dest(dist + 'svg'))
+        .pipe(browserSync.stream());
+
+});
+
+
+/* ================
+// Sync Changes
+// ============= */
+
+gulp.task('browser-sync', function () {
+
+    browserSync.init({
+        logPrefix: packageJSON.name,
+        ui: false,
+        server: {
+            baseDir: './dist'
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+                padding: '4px 8px',
+                fontSize: '12px',
+                borderBottomLeftRadius: '0'
+            }
+        }
+    });
+
+});
+
+gulp.task('reload', function () {
+
+    browserSync.reload();
+
+});
+
+
+/* ================
+// Reset Build
+// ============= */
+
+gulp.task('reset', function () {
+
+    try {
+        packageJSON = reload('./package.json');
+    } catch (e) {
+        console.error("Failed to reload package.json! Error: ", e);
+    }
+
+});
+
+
+/* ================
+// Clean Dist
+// ============= */
+
+gulp.task('clean', function () {
+
+    del.sync([
+        'dist/**',
+        '!dist',
+        '!dist/js',
+        '!dist/css'
+    ]);
+
+});
+
+
+/* ================
+// Watch Files
+// ============= */
+
+gulp.task('watch', function () {
+
+    gulp.watch('package.json', ['reset', 'build']);
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('src/js/**/*.js', ['js']);
+    gulp.watch('src/assets/img/**/*', ['images']);
+    gulp.watch(['src/**/*.hbs', 'src/**/data.json'], ['compile-templates', 'reload']);
+    gulp.watch('src/assets/svg/*', ['sprite']);
+    gulp.watch(['src/**/*.html', 'src/**/*.hbs', 'src/**/*.json'], ['move-templates', 'reload']);
+    // gulp.watch(['src/assets/**', '!src/assets/img'], ['move-assets', 'reload'])
+    gulp.watch(['src/assets/**', '!src/assets/img'])
+
+});
+
+
+/* ================
+// Gulp Task Sets
+// ============= */
+
+gulp.task('build', [
+    'clean',
+    'images',
+    'sprite',
+    'sass',
+    'move-templates',
+    // 'move-assets',
+    'js'
+]);
+
+gulp.task('default', [
+    'build',
+    'compile-templates',
+    'reload',
+    'watch',
+    'browser-sync'
+]);
